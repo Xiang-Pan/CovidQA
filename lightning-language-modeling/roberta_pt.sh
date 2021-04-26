a7ea36d98e03de41d9b868bca66457fbae1c7c9f
XiangPan <xiangpan@nyu.edu>
2021-04-26 17:39:30 -0400

    README update

---
+++ lightning-language-modeling/roberta_pt.sh
@@ -0,0 +1,7 @@
+python run_mlm.py \
+    --model_name_or_path roberta-base \
+    --train_file  \
+    --validation_file path_to_validation_file \
+    --do_train \
+    --do_eval \
+    --output_dir /tmp/test-mlm
